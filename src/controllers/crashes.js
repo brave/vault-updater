a8484d6e75eb5b946bcae7b9539f3afe702c13f7
Aubrey Keus <aubrey@brave.com>
2016-01-06 21:17:45 -0500

    Prep. for Heroku deploy and testing
    Auditor: @bbondy

--- src/controllers/crashes.js
+++ src/controllers/crashes.js
@@ -1,14 +1,31 @@
-exports.setup = () => {
-  // TODO configure S3 connection to store crashes
+var AWS = require('aws-sdk')
 
+AWS.config.region = process.env.AWS_REGION || 'us-west-2'
+var bucketName = process.env.AWS_BUCKET_NAME || 'brave-laptop-crash-reports'
+var s3 = new AWS.S3()
+
+exports.setup = () => {
   let post = {
     method: 'POST',
     path: '/1/crashes',
     config: {
       handler: function (request, reply) {
-        // TODO send crash report to S3
-        console.log('Crash ' + request)
-        reply('OK')
+        // TODO format and send crash report to S3
+        var payload = {
+          Bucket: bucketName,
+          Key: 'some-key',
+          Body: 'This is the body'
+        }
+        s3.upload(payload, (err, results) => {
+          console.log('Crash ' + request)
+          if (err) {
+            console.log(err.toString())
+            console.log(results)
+            reply(err.toString()).code(500)
+          } else {
+            reply('OK')
+          }
+        })
       }
     }
   }
