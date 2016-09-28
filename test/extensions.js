ae4d2464a607bccc8c6d092e782ec8ed2ac5f262
Brian R. Bondy <netzen@gmail.com>
2016-09-27 21:28:01 -0400

    Use our own forked pdfjs extension
    We had some changes for bugfixes that are needed, so we'll maintain our own extension with our own ID for pdfjs
    
    Auditors: @aekeus

--- test/extensions.js
+++ test/extensions.js
@@ -23,7 +23,7 @@ const onePasswordAndPDFJSRequest = (onePasswordVersion, pdfJSVersion) => `
     <updatecheck />
     <ping rd="-2" ping_freshness="" />
   </app>
-  <app appid="oemmndcbldboiebfnladdacbdfmadadm" version="${pdfJSVersion}" installsource="ondemand">
+  <app appid="jdbefljfgobbmcidnmpjamcbhnbphjnb" version="${pdfJSVersion}" installsource="ondemand">
     <updatecheck />
     <ping rd="-2" ping_freshness="" />
   </app>
@@ -45,7 +45,7 @@ const unsupportedProtocolRequest = `
 const availableExtensions = [
   ['aomjjhallfgjeglblehebfpbcfeobpgk', '4.5.9.90', 'f75d7808766429ec63ec41d948c1cb6a486407945d604961c6adf54fe3f459b7'],
   // PDFJS
-  ['oemmndcbldboiebfnladdacbdfmadadm', '1.5.294', '499e05d5cde9a1e735e29fa49af7839690f34eb27a3d952b8e4396ea50c77526'],
+  ['jdbefljfgobbmcidnmpjamcbhnbphjnb', '1.5.444', '25689984431ca8a60f087c761f472e500a7fe8a9065a4a47e92559237bcd1d6d'],
   // Dashlane
   ['fdjamakpfbbddfjaooikfcpapjohcfmg', '4.2.4', '0be29a787290db4c554fd7c77e5c45939d2161688b6cb6b51d39cdedb9cc69d4'],
   // LastPass
@@ -55,7 +55,7 @@ const availableExtensions = [
 tap.test('Extracts extension information from requests', (test) => {
   tap.same(getRequestedExtensions(onePasswordRequest('0.0.0.0')), [['aomjjhallfgjeglblehebfpbcfeobpgk', '0.0.0.0']])
   tap.same(getRequestedExtensions(onePasswordRequest('4.5.9.90')), [['aomjjhallfgjeglblehebfpbcfeobpgk', '4.5.9.90']])
-  tap.same(getRequestedExtensions(onePasswordAndPDFJSRequest('4.5.9.90', '1.5.294')), [['aomjjhallfgjeglblehebfpbcfeobpgk', '4.5.9.90'], ['oemmndcbldboiebfnladdacbdfmadadm', '1.5.294']])
+  tap.same(getRequestedExtensions(onePasswordAndPDFJSRequest('4.5.9.90', '1.5.444')), [['aomjjhallfgjeglblehebfpbcfeobpgk', '4.5.9.90'], ['jdbefljfgobbmcidnmpjamcbhnbphjnb', '1.5.444']])
   tap.equal(getRequestedExtensions(unsupportedProtocolRequest), undefined)
   test.end()
 })
@@ -92,7 +92,7 @@ tap.test('Update for multiple extensions works', (test) => {
   tap.same(getExtensionsWithUpdates(availableExtensions, getRequestedExtensions(onePasswordAndPDFJSRequest('0.0.0.0', '0.0.0.0'))),
     [
       ['aomjjhallfgjeglblehebfpbcfeobpgk', '4.5.9.90', 'f75d7808766429ec63ec41d948c1cb6a486407945d604961c6adf54fe3f459b7'],
-      ['oemmndcbldboiebfnladdacbdfmadadm', '1.5.294', '499e05d5cde9a1e735e29fa49af7839690f34eb27a3d952b8e4396ea50c77526']
+      ['jdbefljfgobbmcidnmpjamcbhnbphjnb', '1.5.444', '25689984431ca8a60f087c761f472e500a7fe8a9065a4a47e92559237bcd1d6d']
     ])
   test.end()
 })
