151ea0eb895c5dc0047e6abb6cbe9533356ff7cd
Aubrey Keus <aubrey@brave.com>
2017-06-25 20:49:30 -0400

    Database migrations

---
+++ migrations/0001-releases/up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE releases (
+  channel      TEXT      NOT NULL,
+  platform     TEXT      NOT NULL,
+  version      TEXT      NOT NULL,
+  name         TEXT      NOT NULL,
+  pub_date     TEXT      NOT NULL,
+  notes        TEXT      NOT NULL,
+  preview      BOOLEAN   NOT NULL,
+  url          TEXT,
+  ts           TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (channel, platform, version)
+);
+
+ALTER TABLE releases ADD CONSTRAINT version_format CHECK ( version ~ '^\d+\.\d+\.\d+$' );
+ALTER TABLE releases ADD CONSTRAINT valid_platforms CHECK ( platform IN ( 'osx', 'winx64', 'winia32', 'linux' ) );
+ALTER TABLE releases ADD CONSTRAINT valid_channels CHECK ( channel IN ( 'dev', 'beta', 'stable' ) );
