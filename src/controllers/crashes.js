89f8825f894361ef71a4aa54857c1a2d2c76c0fa
Aubrey Keus <aubrey@brave.com>
2018-01-23 11:08:44 -0500

    Add a crash_id response to crashes for muon versions > 4.7.3
    Auditors: @bridiver, @bbondy

--- src/controllers/crashes.js
+++ src/controllers/crashes.js
@@ -1,6 +1,8 @@
 let assert = require('assert')
 let Joi = require('joi')
 let logger = require('logfmt')
+let semver = require('semver')
+let randomstring = require('randomstring')
 
 exports.setup = (runtime) => {
   let post = {
@@ -8,10 +10,21 @@ exports.setup = (runtime) => {
     path: '/1/crashes',
     config: {
       handler: function (request, reply) {
-        console.log('Crash report start of processing')
-        reply('OK')
+        let crash_id = 'OK'
+        try {
+          if (request.payload && request.payload['muon-version'] && semver.gt(request.payload['muon-version'], '4.7.3')) {
+            crash_id = randomstring.generate({
+              length: 16,
+              charset: 'hex'
+            })
+          }
+        } catch (e) {
+          // most likely invalid muon version - ignore and send OK
+        }
+        reply(crash_id)
         const payload = request.payload
         payload.ts = (new Date()).getTime()
+        payload.crash_id = crash_id
         runtime.mongo.models.insertCrash(payload, (err, results) => {
           console.log(`crash recorded for version ${payload.ver}`)
         })
