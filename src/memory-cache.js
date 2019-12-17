bdd92381820812b28a60256baece195c2d4c2afc
Aubrey Keus <aekeus@gmail.com>
2019-12-17 15:44:23 -0500

    Added comments to memory cache module

--- src/memory-cache.js
+++ src/memory-cache.js
@@ -8,13 +8,20 @@ const cache = {}
  * function B, update the memory cache and return the result.
  */
 const create = (seconds, func) => {
+  // define variables to hold timestamp of next cache update and
+  // the value to be cached
   let nextRun, v
+  // return function
   return async () => {
+    // if the cache has not been initialized or is due for a refresh
     if (!nextRun || (new Date()).getTime() > nextRun) {
       if (process.env.DEBUG) console.log('refilling cache')
+      // re-fill the cache
       v = await func()
+      // schedule timestamp for next update
       nextRun = (new Date()).getTime() + (seconds * 1000)
     }
+    // return cached value
     return v
   }
 }
