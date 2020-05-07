99ab40f5fe4ff7642bcfe90819b58022235b8c4a
Kevin Kuehler <keur@xcf.berkeley.edu>
2020-05-07 13:24:42 -0700

    Use first IP address as client in x-forwarded-for

--- test/ip.js
+++ test/ip.js
@@ -21,5 +21,32 @@ tap.test('retrieve IP address', function (t) {
     }
   }
   t.ok(common.ipAddressFrom(requestMock) === '9.9.9.9', 'retrieve remote ip address')
+
+  requestMock = {
+    headers: {
+      'x-forwarded-for': '1.1.1.1, 2.2.2.2, 3.3.3.3'
+    }
+  }
+  t.ok(common.ipAddressFrom(requestMock) === '1.1.1.1', 'first ip is client address')
+
+  requestMock = {
+    headers: {
+      'x-forwarded-for': ','
+    },
+    info: {
+      remoteAddress: '9.9.9.9'
+    }
+  }
+  t.ok(common.ipAddressFrom(requestMock) === '9.9.9.9', 'Use remote address on malformed header')
+
+  process.env.X_FORWARDED_NONSTANDARD = 1
+  /* Note that BEHIND_FASTLY is set as part of the testing environment */
+  requestMock = {
+    headers: {
+      'x-forwarded-for': '1.1.1.1, 2.2.2.2, 3.3.3.3'
+    }
+  }
+  t.ok(common.ipAddressFrom(requestMock) === '2.2.2.2', 'second ip is client address')
+  delete process.env.X_FORWARDED_NONSTANDARD
   t.end()
 })
